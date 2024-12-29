a3da490d8cff8caa4c103d37492559bad4740af2
MINYAZ07 <hbye17848@gmail.com>
2024-12-28 21:56:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function mostrarAnillo() {
-    const anillo = document.getElementById("anillo");
-    anillo.classList.remove("anillo-oculto");
-    anillo.classList.add("anillo-visible");
-
-    setTimeout(() => {
-        alert("¡Gracias por aceptar! Este anillo es el comienzo de nuestra aventura juntos ❤️");
-    }, 2000);
+    const anillo = document.getElementById('anillo');
+    anillo.classList.remove('anillo-oculto');
+    anillo.classList.add('anillo-visible');
 }
